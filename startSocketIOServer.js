c5b5a5b69024b44e5f4434e7ce8d0697002b47e6
monosniper <ravilto@vk.com>
2023-10-30 23:05:35 +0300

    sdg

--- startSocketIOServer.js
+++ startSocketIOServer.js
@@ -59,23 +59,19 @@ const startSocketIOServer = (httpServer) => {
 		    console.error('Use 600 sec')
 		}
 
-		// Start pressed
-		socket.on("start", async ({ data }) => {
-		    if(user.demo_time >= demo_time) {
-			socket.emit("demo_expired")
-		    } else {
+		function start() {
 			const checks = []
 			const founds = []
 			const logs = []
 
 			// Create session
 			const session = await Session.create({
-			    user_id: socket.data.user.id,
-			    data: {
-				logs: [],
-				checks: [],
-				founds: [],
-			    }
+				user_id: socket.data.user.id,
+				data: {
+					logs: [],
+					checks: [],
+					founds: [],
+				}
 			})
 
 			socket.data.session_id = session.id
@@ -83,112 +79,120 @@ const startSocketIOServer = (httpServer) => {
 			const getRandomCoin = () => data[Math.floor(Math.random()*data.length)]
 
 			function check(name) {
-			    setTimeout(() => {
-				if(socket.data.session_id) {
-				    checks.push({
-					name: name.toUpperCase(),
-					text: "Wallet Check: " + generateWallet()
-				    })
-
-				    function low() {
-					// $ 1 - 15
-					return randomIntFromInterval(10, 150) / 10
-				    }
-
-				    function middle() {
-					// $ 15 - 100
-					return randomIntFromInterval(150, 1000) / 10
-				    }
-
-				    function high() {
-					// $ 100 - 500
-					return randomIntFromInterval(1000, 5000) / 10
-				    }
-
-				    function freeLow() {
-					// $ 0.01 - 0.1
-					return randomIntFromInterval(0.1, 1) / 10
-				    }
-
-				    function freeMiddle() {
-					// $ 0.1 - 5
-					return randomIntFromInterval(1, 50) / 10
-				    }
-
-				    const list = socket.data.user.status === 'pro' ? [
-					{chance: 0.1, func: low},
-					{chance: 0.01, func: middle},
-					{chance: 0.001, func: high}
-				    ] : [
-					{chance: 0.01, func: freeLow},
-					{chance: 0.0002, func: freeMiddle},
-				    ];
-
-				    function callRandomFunction(list) {
-					const rand = Math.random() // get a random number between 0 and 1
-					let accumulatedChance = 0 // used to figure out the current
-
-					const found = list.find(function(element) { // iterate through all elements
-					    accumulatedChance += element.chance // accumulate the chances
-					    return accumulatedChance >= rand // tests if the element is in the range and if yes this item is stored in 'found'
-					})
-
-					if( found ) {
-					    const amount = found.func()
-
-					    if(amount) {
-						founds.push({
-						    name: name.toUpperCase(),
-						    amount
+				setTimeout(() => {
+					if(socket.data.session_id) {
+						checks.push({
+							name: name.toUpperCase(),
+							text: "Wallet Check: " + generateWallet()
 						})
-					    }
+
+						function low() {
+							// $ 1 - 15
+							return randomIntFromInterval(10, 150) / 10
+						}
+
+						function middle() {
+							// $ 15 - 100
+							return randomIntFromInterval(150, 1000) / 10
+						}
+
+						function high() {
+							// $ 100 - 500
+							return randomIntFromInterval(1000, 5000) / 10
+						}
+
+						function freeLow() {
+							// $ 0.01 - 0.1
+							return randomIntFromInterval(0.1, 1) / 10
+						}
+
+						function freeMiddle() {
+							// $ 0.1 - 5
+							return randomIntFromInterval(1, 50) / 10
+						}
+
+						const list = socket.data.user.status === 'pro' ? [
+							{chance: 0.1, func: low},
+							{chance: 0.01, func: middle},
+							{chance: 0.001, func: high}
+						] : [
+							{chance: 0.01, func: freeLow},
+							{chance: 0.0002, func: freeMiddle},
+						];
+
+						function callRandomFunction(list) {
+							const rand = Math.random() // get a random number between 0 and 1
+							let accumulatedChance = 0 // used to figure out the current
+
+							const found = list.find(function(element) { // iterate through all elements
+								accumulatedChance += element.chance // accumulate the chances
+								return accumulatedChance >= rand // tests if the element is in the range and if yes this item is stored in 'found'
+							})
+
+							if( found ) {
+								const amount = found.func()
+
+								if(amount) {
+									founds.push({
+										name: name.toUpperCase(),
+										amount
+									})
+								}
+							}
+						}
+
+						callRandomFunction(list)
+
+						const newData = {checks, founds, logs}
+
+						socket.emit("update", newData)
+
+						session.data = newData
+						session.save()
+
+						check(getRandomCoin())
 					}
-				    }
-
-				    callRandomFunction(list)
-
-				    const newData = {checks, founds, logs}
-
-				    socket.emit("update", newData)
-
-				    session.data = newData
-				    session.save()
-
-				    check(getRandomCoin())
-				}
-			    }, socket.data.user.status === 'pro' ? 50 : 500)
+				}, socket.data.user.status === 'pro' ? 50 : 500)
 			}
 
 			check(getRandomCoin())
 
 			// event for client to start
 			socket.emit("started")
-		    }
+		}
+
+		// Start pressed
+		socket.on("start", async ({ data }) => {
+			if(user.status === 'demo') {
+				if(user.demo_time >= demo_time) {
+					socket.emit("demo_expired")
+				} else start()
+			} else start()
 
 		    console.log('Start event')
 		});
 
 		socket.on('stop', async () => {
 		    if(socket.data.session_id) {
-			const session = await Session.findOne({where: {id: socket.data.session_id}})
+				const session = await Session.findOne({where: {id: socket.data.session_id}})
 
-			user.demo_time = user.demo_time + session.createdAt.getSeconds()
-			await user.save()
+				user.demo_time = user.demo_time + session.createdAt.getSeconds()
+				await user.save()
 
-			await session.destroy()
+				await session.destroy()
 
-			socket.data.session_id = null
+				socket.data.session_id = null
 
-			// event for client to stop
-			socket.emit("stopped")
+				// event for client to stop
+				socket.emit("stopped")
 
-			if(user.demo_time >= demo_time) {
-			    socket.emit("demo_expired")
-			}
+				if(user.demo_time >= demo_time) {
+					socket.emit("demo_expired")
+				}
 		    }
 		})
 	    } else {
-		socket.disconnect()
+			socket.disconnect()
 	    }
 	}
 
